fc0a98c3272fb297788a8b772caa14deef73ec63
Aimée <56592470+aimeemalzac@users.noreply.github.com>
2020-04-26 09:41:21 -0300

    dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>DCTA</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/dashboard/">
+
+    <!-- Bootstrap core CSS -->
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+
+
+    <?php
+
+session_start();
+
+$usuario = $_SESSION['usuario'];
+
+if (!isset($_SESSION['usuario'])) {
+  header ('Location: index.php');
+}
+
+
+
+include 'conexao.php';
+
+$sql = "SELECT nivel_usuario FROM usuarios WHERE mail_usuario ='$usuario' and status = 'Ativo'";
+
+$buscar = mysqli_query($conexao, $sql);
+
+$array = mysqli_fetch_array($buscar);
+
+$nivel_usuario = $array['nivel_usuario'];
+
+?> 
+
+
+  <nav class="navbar navbar-dark  bg-primary flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">DCTA</a>
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="#">Sign out</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <span data-feather="home"></span>
+              Relatórios <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pagina=servidoresOm">
+              <span data-feather="file"></span>
+              Servidores por Organização Militar
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pagina=servidoresNivel">
+              <span data-feather="shopping-cart"></span>
+              Servidores por Nível de Escolaridade
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pagina=servidoresAposentaveis">
+              <span data-feather="users"></span>
+              Servidores Aposentáveis
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Relatório</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group mr-2">
+            
+          </div>
+          
+        </div>
+      </div>
+
+      <?php
+
+      if(isset($_GET['pagina'])){
+
+      	switch ($_GET['pagina']) {
+      		case 'servidoresOm':
+      			echo '<h2>Servidores por Organização Militar</h2>';
+      			include 'dash/graficos/terceiroGrafico.php';
+      			break;
+
+      		case 'servidoresNivel':
+      			echo '<h2>Servidores por Nível de Escolaridade</h2>';
+            include 'dash/graficos/segundoGrafico.php';
+      			break;
+
+      		case 'servidoresAposentaveis':
+      			echo '<h2>Aposentáveis por ano</h2>';
+            include 'dash/graficos/primeiroGrafico.php';
+      			break;
+      		
+      		default:
+      			echo "NENHUMA OPÇÃO ESCOLHIDA";
+      			break;
+      	}
+
+
+      }
+
+
+      ?>
+
+    
+
+     
+      
+    </main>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script></body>
+</html>
