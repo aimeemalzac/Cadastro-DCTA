e04d6023a30c74aa34e691d20f3bd87a19c3014f
Aimée <56592470+aimeemalzac@users.noreply.github.com>
2020-05-10 15:36:23 -0300

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,52 +1,23 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.6">
-    <title>DCTA</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/dashboard/">
-
-    <!-- Bootstrap core CSS -->
-
+<head>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
-<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
-<meta name="theme-color" content="#563d7c">
-
-
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+      
+      @media only screen and (max-width: 600px) {
+        body{
+          width: 300 px;
         }
       }
     </style>
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-  </head>
-  <body>
+    
+</head>
+  
+
+<body>
 
 
 <?php
@@ -85,7 +56,7 @@ $nivel_usuario = $array['nivel_usuario'];
 
 <div class="container-fluid">
   <div class="row">
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar" style='background-color: #E9E9E9 '>
       <div class="sidebar-sticky">
         <ul class="nav flex-column">
           <li class="nav-item">
@@ -110,6 +81,18 @@ $nivel_usuario = $array['nivel_usuario'];
             <a class="nav-link" href="?pagina=servidoresAposentaveis">
               <span data-feather="users"></span>
               Servidores Aposentáveis
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pagina=servidoresAposentaveisFim">
+              <span data-feather="users"></span>
+              Servidores Aposentáveis - Área Fim 
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pagina=servidoresAposentaveisMeio">
+              <span data-feather="users"></span>
+              Servidores Aposentáveis - Área Meio
             </a>
           </li>
         </ul>
@@ -153,6 +136,16 @@ $nivel_usuario = $array['nivel_usuario'];
       			echo '<h2>Aposentáveis por ano</h2>';
             include 'dash/graficos/primeiroGrafico.php';
       			break;
+
+          case 'servidoresAposentaveisFim':
+            echo '<h2>Aposentáveis por ano</h2>';
+            include 'dash/graficos/quartoGrafico.php';
+            break;
+
+          case 'servidoresAposentaveisMeio':
+            echo '<h2>Aposentáveis por ano</h2>';
+            include 'dash/graficos/quintoGrafico.php';
+            break;
       		
       		default:
       			echo "NENHUMA OPÇÃO ESCOLHIDA";
