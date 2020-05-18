e4b6be66c4a3523936b0b02ec4e56431d351c303
Aimée <56592470+aimeemalzac@users.noreply.github.com>
2020-05-18 10:22:08 -0300

    _listar.php

--- _listar.php
+++ _listar.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Listagem dos servidores</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-	<script src="https://kit.fontawesome.com/90ee13269e.js" crossorigin="anonymous"></script>
-
-</head>
-
-<body>
-
 <?php
 
 session_start();
@@ -32,6 +20,35 @@ $array = mysqli_fetch_array($buscar);
 $nivel_usuario = $array['nivel_usuario'];
 
 ?> 
+
+
+<!DOCTYPE html>
+<html>
+
+<head>   
+<meta charset="utf-8">   
+
+<title></title>  
+
+<meta name="description" content="Bootstrap."> 
+
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">  
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
+
+<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+
+<script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<script src="https://kit.fontawesome.com/90ee13269e.js" crossorigin="anonymous"></script>
+
+</head> 
+
+<body>
+
+
 	
 
 <div class= "container" style="margin-top: 40px">
@@ -41,7 +58,7 @@ $nivel_usuario = $array['nivel_usuario'];
 </div>
 	<h3>Lista de Servidores</h3>
 <br>
-	<table class="table">
+	<table id ="dados" class="table">
 		  <thead>
 		    <tr>
 		      <th scope="col">Nome</th>
@@ -56,6 +73,7 @@ $nivel_usuario = $array['nivel_usuario'];
 	    	include 'conexao.php';
 
 	    	$sql = "SELECT *, TIMESTAMPDIFF (YEAR, `datanascimento`, CURDATE()) AS idade FROM dados";
+	    	
 	    	$busca = mysqli_query($conexao, $sql);
 
 
@@ -63,8 +81,6 @@ $nivel_usuario = $array['nivel_usuario'];
 
 			$b = mysqli_query($conexao, $nsql);
 	    	
-
-
 
 	    	while ($array = mysqli_fetch_array($busca)) {
 
@@ -105,9 +121,6 @@ $nivel_usuario = $array['nivel_usuario'];
 			
 			<?php {
 
-
-			//mysql_data_seek( $, 0 );
-
 			while ($array = mysqli_fetch_array($b)) {
 
 	    		$id = $array ['id'];
@@ -144,6 +157,9 @@ $nivel_usuario = $array['nivel_usuario'];
 				$datatitulacao = $array ['datatitulacao'];
 				$abono = $array['abono'];
 				?>
+ 
+				
+
 
 
 	 		<tr>
@@ -173,6 +189,7 @@ $nivel_usuario = $array['nivel_usuario'];
 		 		</td>
 	 		</tr>
 
+			
 			<?php } ?>
 			<?php } ?>
 			<?php } ?>
@@ -182,10 +199,56 @@ $nivel_usuario = $array['nivel_usuario'];
 
 </div>
 
+<script>
+$(document).ready(function(){
+    $('#dados').dataTable( {
+		"language": {
+    "sEmptyTable": "Nenhum registro encontrado",
+    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sInfoThousands": ".",
+    "sLengthMenu": "_MENU_ Resultados por página",
+    "sLoadingRecords": "Carregando...",
+    "sProcessing": "Processando...",
+    "sZeroRecords": "Nenhum registro encontrado",
+    "sSearch": "Pesquisar",
+    "oPaginate": {
+        "sNext": "Próximo",
+        "sPrevious": "Anterior",
+        "sFirst": "Primeiro",
+        "sLast": "Último"
+    },
+    "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+    },
+    "select": {
+        "rows": {
+            "_": "Selecionado %d linhas",
+            "0": "Nenhuma linha selecionada",
+            "1": "Selecionado 1 linha"
+        }
+    },
+    "buttons": {
+        "copy": "Copiar para a área de transferência",
+        "copyTitle": "Cópia bem sucedida",
+        "copySuccess": {
+            "1": "Uma linha copiada com sucesso",
+            "_": "%d linhas copiadas com sucesso"
+        }
+    }
+}
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		});
+});
+</script>
+
+
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
 
 </body>
 
